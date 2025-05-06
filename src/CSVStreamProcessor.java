8f8be94f1d7b6bab30256867403e0a3a11a3c791
Yvette IRADUKUNDA <iradyvette98@gmail.com>
2025-05-06 16:53:29 +0200

    Adding changes

--- src/CSVStreamProcessor.java
+++ src/CSVStreamProcessor.java
@@ -5,13 +5,27 @@ import java.util.stream.Stream;
 
 public class CSVStreamProcessor {
     public static double calculateAverageAge(String csvFilePath) throws IOException {
-        Files.lines(Paths.get(csvFilePath));
-        Stream<String> Lines = Files.lines(Paths.get(csvFilePath));
-        return Lines
-                .skip(1)
-                .filter(Age -> Age != null)
-                .map(line -> line.split(","))
-                .mapToInt(fields ->Integer.parseInt(fields[1]))
-                .average().orElse(0.0);
+
+        try (Stream<String> Lines = Files.lines(Paths.get(csvFilePath))) {
+            return Lines
+                    .skip(1)
+                    .filter(Age -> Age != null)
+                    .map(line -> line.split(","))
+                    .mapToInt(fields -> Integer.parseInt(fields[1]))
+                    .average().orElse(0.0);
+        }
+        catch (NumberFormatException e)
+
+        {
+            System.out.println("The CSV file contains an invalid number"+e.getMessage());
+            return 0;
+        }
     }
+
+
+public static void main(String[] args) throws IOException {
+    double averageAge = calculateAverageAge("D:\\VS CODE Projects\\Java Practices\\Week 5\\Assignments\\src\\People.csv");
+    System.out.println("Average Age = " + String.format("%.2f", averageAge));
+}
+
 }
